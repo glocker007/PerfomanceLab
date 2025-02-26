4afc0a1cc6599fe50f540fcf3ec0bc35b721c90e
glocker007 <glocker007@mail.ru>
2025-02-27 02:06:46 +0300

    little correction task 1

--- task1/task1.py
+++ task1/task1.py
@@ -1,11 +1,14 @@
 from math import gcd
 n, m= map(int, input().split())
 num = gcd(m - 1, n)
-k = n//num # k * (m-1) = 0 mod n
-def mod(k, n):
-    if (k%n == 0):
-         return n
-    else:
-         return k%n
-result = ''.join([str(mod((1 + i*(m-1)), n)) for i in range(k) ])
-print(result)
+if (m == 1):
+     print(1)
+else:
+     k = n//num # k * (m-1) = 0 mod n
+     def mod(k, n):
+          if (k%n == 0):
+               return n
+          else:
+               return k%n
+     result = ''.join([str(mod((1 + i*(m-1)), n)) for i in range(k) ])
+     print(result)
